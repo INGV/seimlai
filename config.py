a42481dad5d448791a063cc625589f726a833db6
Rossella Fonzetti <rossella.fonzetti@192.168.1.38>
2026-04-01 09:45:32 +0200

    modifica nomi dir

--- config.py
+++ config.py
@@ -175,10 +175,10 @@ inventory_dir = os.path.join(project_root, "inventory")
 waveform_base = os.path.join(root_dir, str(year))  # where script 02+ reads from
 output_base = os.path.join(project_root, "output")
 output_picks_dir = os.path.join(output_base, f"output_picks_{P_THRESHOLD}")
-plot_dir = os.path.join(output_base, "plots_annotations", f"{year}_{start_day:03d}_{end_day:03d}")
+plot_dir = os.path.join(output_base, f"plots_annotations_{P_THRESHOLD}", f"{year}_{start_day:03d}_{end_day:03d}")
 log_file_path = os.path.join(output_base, "phase_picking_log.txt")  # used by scripts 02+
 # Used by script 04_1 for the output
-output_dir = os.path.join(output_base, "output_catalog")
+output_dir = os.path.join(output_base, f"output_catalog_{P_THRESHOLD}")
 # Used by script 05 and script 06 for the filtered Hypoellipse output
 h71_filtered_dir = os.path.join(output_dir, "filtered_data")
 # Additional path needed for some scripts
